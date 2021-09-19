387d199b5443964cb57c3263c019dc5182201ddf
Adamets Vladislav <v.adamets@consult-info.ru>
2021-09-20 00:20:26 +0200

    chore: init commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+    $APPLICATION->SetTitle("Новости банка");
+?>
+
+Content
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php"); ?>
